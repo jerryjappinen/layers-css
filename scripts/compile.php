7df46a4137104038b79230b802726f5ee667c714
Jerry Jäppinen <eiskis@gmail.com>
2013-09-12 12:54:19 +0300

    Added appropriate comments to release files

--- scripts/compile.php
+++ scripts/compile.php
@@ -13,10 +13,9 @@ function minify($string) {
 // Choose what to include
 $sourcePath = '../source/';
 $releasePath = '../release/';
+$name = 'layers';
 if (isset($_GET['path']) and is_dir($sourcePath.$_GET['path'].'/')) {
-	$path = $sourcePath.$_GET['path'].'/';
-} else {
-	$path = $sourcePath;
+	$name = $_GET['path'];
 }
 
 
@@ -24,18 +23,20 @@ if (isset($_GET['path']) and is_dir($sourcePath.$_GET['path'].'/')) {
 // Content type header
 header('Content-Type: text/css; charset: UTF-8');
 
-// Include all files
-$output = '';
-foreach (rglob($path.'*') as $value) {
+// Include files
+$output = '/*
+Layers CSS'.($name === 'responsive' ? ' responsive adjustments' : '').' by Jerry Jäppinen
+Released under the MIT license
+http://eiskis.net/layers
+Compiled from source on '.date('Y-m-d H:i e') .'
+*/
+';
+foreach (rglob($sourcePath.$name.'/'.'*') as $value) {
 	$output .= minify(file_get_contents($value));
 }
 
 // Save compilation file
 if (isset($_GET['save'])) {
-	$name = basename($path);
-	if (empty($name)) {
-		$name = 'all';
-	}
 	file_put_contents($releasePath.$name.'.css', $output);
 }
 
